3019c1afb68b6ed0cfa25219e5f6ab91666a8145
suot00 <truongkhacsuot661@gmail.com>
2022-07-13 19:41:00 +0700

    up

--- scripts.js
+++ scripts.js
@@ -1,45 +1,64 @@
 let _value = document.querySelector('.container__value');
 
-// const btnDecrease= document.querySelector('.btn--decrease');
-// const btnReset= document.querySelector('btn--reset');
-// const btnIncrease= document.querySelector('btn--increase');
+// let btns = document.querySelectorAll('.container__btn button');
+
 
 function decreaseValue() {
     let counterValue = _value.textContent;
     let updatedValue = counterValue - 1;
-    if (updatedValue > 0) {
-        _value.style.color = "green";
-    }
-    else if (updatedValue < 0) {
-        _value.style.color = "red";
-    }
-    else {
-        _value.style.color = "black";
-    }
     _value.textContent = updatedValue;
+    changeColor(updatedValue)
   }
 
-  function increaseValue() {
+function increaseValue() {
     let counterValue = _value.textContent;
     let updatedValue = parseInt(counterValue) + 1;
-    if (updatedValue > 0) {
+    _value.textContent = updatedValue;
+    changeColor(updatedValue)
+  }
+
+function resetValue() {
+    let updatedValue = 0;
+    _value.textContent=updatedValue;
+     changeColor(updatedValue)
+    
+  }
+
+function changeColor(valueNumber){
+    const changeValue= valueNumber;
+    if (changeValue > 0) {
         _value.style.color = "green";
     }
-    else if (updatedValue < 0) {
+    else if (changeValue < 0) {
         _value.style.color = "red";
     }
     else {
         _value.style.color = "black";
     }
-    _value.textContent = updatedValue;
-  }
+}
 
-  function resetValue() {
-    let counterValue = 0;
-    _value.textContent=counterValue;
-    _value.style.color = "black"
-    
-  }
+// for (i of btns) {
+//   i.addEventListener('click', function() {
+
+//     console.log(this.textContent);
+
+//     if(this.textContent=='Decrease'){
+//         let counterValue = _value.textContent;
+//             let updatedValue = counterValue - 1;
+//             _value.textContent = updatedValue;
+//             changeColor(updatedValue)
+//     }else if(this.textContent=='Increase'){
+//         let counterValue = _value.textContent;
+//         let updatedValue = parseInt(counterValue) + 1;
+//         _value.textContent = updatedValue;
+//         changeColor(updatedValue)
+//     } else {
+//         let updatedValue =0;
+//         _value.textContent = updatedValue;
+//         changeColor(updatedValue)
+//     }
+//   });
+// }
 
 
 
